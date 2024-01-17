4afe7e5b534e5dff393208d37d52d66ee269ec3a
Dileep Kumar Doddi <dileep18052001@gmail.com>
2024-01-18 00:05:57 +0530

    feat: added google OAuth and login page revamp

--- src/App.js
+++ src/App.js
@@ -2,16 +2,27 @@ import Login from './Components/login';
 import SignUp from './Components/signup';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import { useEffect } from 'react';
+import { gapi } from 'gapi-script';
+
+const clientId = "391009188824-nonh55ejjoiv94usp8bh7i83j3rjqa9g.apps.googleusercontent.com";
 
 function App() {
+  useEffect(() => {
+    function start(){
+      gapi.client.init({
+        clientId: clientId,
+        scope: ""
+      })
+    };
+    gapi.load('client:auth2', start)
+  });
+
   return (
     <Router>
       <Routes>
-        {/* <Route path="/" element={<Home />} /> */}
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
-        {/* <Route path="/pq" element={<PersonalQuestions />} />
-        <Route path="*" element={<NotFound />} /> */}
       </Routes>
     </Router>
   );
--- src/Components/login/index.js
+++ src/Components/login/index.js
@@ -1,42 +1,68 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { GoogleLogin } from "react-google-login";
+
+const clientId =
+  "391009188824-nonh55ejjoiv94usp8bh7i83j3rjqa9g.apps.googleusercontent.com";
 
 const Login = () => {
-  const [emailOrPhone, setEmailOrPhone] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const navigate = useNavigate();
 
   const handleLogin = () => {
-    console.log('Logging in with:', emailOrPhone, password);
+    console.log("Logging in with:", email, password);
+  };
+
+  const onSuccess = (res) => {
+    console.log("LOGIN SUCCESS!! curresnt user:", res.profileObj);
+  };
+
+  const onFailure = (res) => {
+    console.log("LOGIN FAILED!! res:", res);
   };
 
   const handleSignUp = () => {
-    navigate('/signup');
+    navigate("/signup");
   };
 
   const handleGoogleSignIn = () => {
-    console.log('Signing in with Google');
+    console.log("Signing in with Google");
   };
 
   return (
-    <div className="flex justify-center items-center h-screen bg-gray-100">
-      <div className="bg-white p-8 rounded-lg shadow-md">
-        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9d/Capgemini_201x_logo.svg/2560px-Capgemini_201x_logo.svg.png" className="mb-2" alt="logo" height={120} width={120}></img>
-        <h2 className="text-3xl font-bold mb-2">Welcome back</h2>
-        <p className='text-gray-400'>Don’t have an account? <span className="text-center text-blue-500 cursor-pointer mt-2 font-semibold" onClick={handleSignUp}>Sign up.</span></p>
-        <div className='flex mt-4 mb-4'>
-          <div className='mr-2'>
+    <div className="md:flex justify-center items-center h-screen bg-white">
+      <div className="bg-white p-8 rounded-lg md:shadow-md">
+        <img
+          src="https://i.ibb.co/c8pFKNw/ramastro-logo-copy.png"
+          className="mb-2"
+          alt="logo"
+          height={120}
+          width={120}
+        ></img>
+        <h2 className="text-2xl font-bold mb-2">Welcome back</h2>
+        <p className="text-gray-400">
+          Book an astrologer in seconds. Don’t have an account?{" "}
+          <span
+            className="text-center text-blue-500 cursor-pointer mt-2 font-semibold"
+            onClick={handleSignUp}
+          >
+            Sign up.
+          </span>
+        </p>
+        <div className="flex mt-4 mb-4">
+          <div className="mr-2">
             <label>Email</label>
             <input
               required
               type="text"
               placeholder="name@example.com"
-              value={emailOrPhone}
-              onChange={(e) => setEmailOrPhone(e.target.value)}
-              className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:border-green-500"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
             />
           </div>
-          <div className='ml-2'>
+          <div className="ml-2">
             <label>Password</label>
             <input
               required
@@ -44,30 +70,88 @@ const Login = () => {
               placeholder="........"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
-              className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:border-green-500"
+              className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
             />
           </div>
         </div>
-        <button
-          onClick={handleLogin}
-          className="w-full bg-green-500 text-white p-3 rounded cursor-pointer hover:bg-green-600 focus:outline-none"
-        >
-          Login
-        </button>
-        <p className="text-center text-blue-500 cursor-pointer mt-2" onClick={handleSignUp}>
-          Sign up
-        </p>
         <div className="flex items-center my-6">
           <div className="flex-grow h-0.5 bg-gray-300"></div>
           <span className="mx-4 text-gray-700">or</span>
           <div className="flex-grow h-0.5 bg-gray-300"></div>
         </div>
-        <button
-          className="w-full bg-blue-500 text-white p-3 rounded cursor-pointer hover:bg-blue-600 focus:outline-none"
+        <GoogleLogin
           onClick={handleGoogleSignIn}
+          className="w-full p-3 border border-gray-300 rounded-lg flex items-center justify-center space-x-2 text-black"
+          clientId={clientId}
+          buttonText="Sign in with Google"
+          onSuccess={onSuccess}
+          onFailure={onFailure}
+          cookiePolicy={"single_host_origin"}
+          isSignedIn={true}
+        ></GoogleLogin>
+        <div className="mt-4 mb-4 flex items-center justify-between">
+          <div className="flex items-center">
+            <input
+              type="checkbox"
+              id="rememberMe"
+              className="mr-2 border border-gray-300 rounded focus:outline-none"
+            />
+            <label htmlFor="rememberMe" className="text-gray-400">
+              Remember me
+            </label>
+          </div>
+          <a href="/" className="text-blue-500 cursor-pointer font-semibold">
+            Forgot password?
+          </a>
+        </div>
+        <button
+          type="submit"
+          onClick={handleLogin}
+          className="w-full bg-blue-500 text-white p-3 rounded cursor-pointer hover:bg-blue-600 focus:outline-none"
         >
-          Sign in with Google
+          Sign in to your account
         </button>
+        <p className="text-sm mt-4">
+          By Signing up, you agree to our{" "}
+          <span className="text-blue-500 cursor-pointer">
+            <a href="/">Terms of Use</a>
+          </span>{" "}
+          and{" "}
+          <span className="text-blue-500 cursor-pointer">
+            <a href="/">Privacy Policy</a>
+          </span>
+          .
+        </p>
+      </div>
+      <div className="ml-4">
+        {/* <img
+          src="https://i.ibb.co/c8pFKNw/ramastro-logo-copy.png"
+          className="mb-2"
+          alt="logo"
+          height={120}
+          width={120}
+        ></img> */}
+        <h1 className="text-4xl font-bold mb-2">
+          Explore the world’s <br></br> leading online astrology.
+        </h1>
+        <p>
+          Countless astrology enthusiasts and seekers turn to our platform{" "}
+          <br></br>the hub for the most skilled and insightful astrologers and
+          online <br></br>astrology services globally.
+        </p>
+        <div className="mt-4 flex">
+          <img
+            src="https://i.ibb.co/zxvyQTC/testimonial-set-of-ppl-copy.png"
+            className="mb-2"
+            alt="happy customers"
+            height={120}
+            width={120}
+          ></img>
+          <p className="pt-2">
+            | &nbsp; Over <span className="font-bold">15.7k</span> Happy
+            Customers
+          </p>
+        </div>
       </div>
     </div>
   );
